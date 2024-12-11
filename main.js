06fa80ef26118aaa4f636a21ad011f95ab5cb061
SophieHornik <sophiehornik07@gmail.com>
2024-12-11 13:17:14 -0500

    Event handling

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const app = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
+            cart:1,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            details: ['50% Cotton', '30% Wool', '20% Polyester'],
-            variants:[
-                {id:2234, color:'green'},
-                {id:2235, color:'blue'},
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods{
+        addToCart(){
+            this.cart += 1
         }
     }
 })
