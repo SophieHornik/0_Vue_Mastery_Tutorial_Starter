6adb3f3909c1d377f247f5d9ab08ba23479f6e65
SophieHornik <sophiehornik07@gmail.com>
2024-12-11 13:07:17 -0500

    list Rendering

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return{
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100
+            inventory: 100,
+            details: ['50% Cotton', '30% Wool', '20% Polyester'],
+            variants:[
+                {id:2234, color:'green'},
+                {id:2235, color:'blue'},
+            ]
         }
     }
 })
